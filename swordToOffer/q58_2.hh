b46a60748a582046aa6d15ebf4774d2676d4605b
Peng Xie <45203165+love1angel@users.noreply.github.com>
2022-08-29 21:31:42 +0800

    [dev] add new solutions

--- swordToOffer/q58_2.hh
+++ swordToOffer/q58_2.hh
@@ -16,12 +16,12 @@ public:
     }
 
 private:
-    void exchange(std::string &s, int start, int end)
+    void exchange(std::string &s, int beg, size_t end)
     {
-        for (int i = 0; i < (end - start) / 2; ++i) {
-            auto temp = s[end - 1 - i];
-            s[end - 1 - i] = s[start + i];
-            s[start + i] = temp;
+        for (int i = 0; i < (end - beg) / 2; ++i) {
+            char temp = s[beg + i];
+            s[beg + i] = s[end - 1 - i];
+            s[end - 1 - i] = temp;
         }
     }
 };
