8b76d195445ea806ee2f8fa2c3bdbff0680df076
Peng Xie <helianthus547@gmail.com>
2024-06-27 17:30:00 +0800

    update Thu Jun 27 17:30:00 CST 2024

--- hot/53/q53.hh
+++ hot/53/q53.hh
@@ -1,47 +1,44 @@
-#pragma once
-
 class Solution {
 public:
     int maxSubArray(vector<int>& nums)
     {
-        int pre = 0, max = std::numeric_limits<int>::min();
-        for (int num : nums) {
-            pre = std::max(num, pre + num);
-            max = std::max(pre, max);
+        int ths {}, sum { std::numeric_limits<int>::min() };
+        for (auto num : nums) {
+            ths = std::max(num, ths + num);
+            sum = std::max(ths, sum);
         }
-        return max;
+        return sum;
     }
 };
 
 class Solution {
     struct Status {
-        int m_left {}; // 左端点始 最大子段和
-        int m_right {}; // 右端点始 最大子段和
-        int m_sum {}; // 区间和
-        int m_max {}; // 最大和 取（左区间最大和，右区间最大和，横跨中间和）
+        int LSum, RSum, iSum, MSum;
     };
 
 public:
     int maxSubArray(vector<int>& nums)
     {
-        return recurse(nums, 0, nums.size() - 1).m_max;
+        return this->get(nums, 0, nums.size() - 1).MSum;
     }
 
 private:
-    Status recurse(std::vector<int>& nums, int left, int right)
+    Status get(const std::vector<int>& nums, int left, int right)
     {
         if (left == right)
             return { nums[left], nums[left], nums[left], nums[left] };
+        int mid = left + ((right - left) >> 1);
+        auto l = this->get(nums, left, mid);
+        auto r = this->get(nums, mid + 1, right);
 
-        int mid = left + (right - left) / 2;
-        Status l = recurse(nums, left, mid);
-        Status r = recurse(nums, mid + 1, right);
+        return this->pushUp(l, r);
+    }
 
-        return {
-            std::max(l.m_left, l.m_sum + r.m_left),
-            std::max(r.m_right, r.m_sum + l.m_right),
-            l.m_sum + r.m_sum,
-            std::max(std::max(l.m_max, r.m_max), l.m_right + r.m_left)
-        };
+    Status pushUp(const Status& l, const Status& r)
+    {
+        auto lsum = std::max(l.LSum, l.iSum + r.LSum);
+        auto rsum = std::max(r.RSum, r.iSum + l.RSum);
+        auto isum = l.iSum + r.iSum;
+        return { lsum, rsum, isum, std::max(std::max(l.MSum, r.MSum), l.RSum + r.LSum) };
     }
 };
